e2822832c05cbfbe3f0232481086d1379ec421cb
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-06-04 23:26:35 +0100

    added activites table to DB

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -9,6 +9,7 @@ namespace Persistance
         public DataContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder) {
             builder.Entity<Value>().HasData(
@@ -16,8 +17,6 @@ namespace Persistance
                 new Value {Id = 2, Name = "Value 102"},
                 new Value {Id = 3, Name = "Value 103"}
             );
-
-            // Console.WriteLine("This is C#");
         }
         
     }
