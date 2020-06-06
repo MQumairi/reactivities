8368e227d64d6103d4294e89729994cc556cb938
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-06-06 12:23:50 +0100

    edit handler done

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -31,14 +31,18 @@ namespace API.Controllers
             return await mediator.Send(new Details.Query{Id = id});
         }
 
-
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await mediator.Send(command);
         }
 
-
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await mediator.Send(command);
+        }
 
     }
 
---
+++ Application/Activites/Edit.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Persistance;
+
+namespace Application.Activites
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public String Title { get; set; }
+            public String Description { get; set; }
+            public String Category { get; set; }
+            public DateTime? Date { get; set; }
+            public String City { get; set; }
+            public String Venue { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext context;
+            public Handler(DataContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await context.Activities.FindAsync(request.Id);
+                if(activity == null) throw new Exception("No activity found");
+
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
+
+                var success = await context.SaveChangesAsync() > 0;
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem adding activity");
+            }
+        }
+    }
+}
